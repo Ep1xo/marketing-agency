36512a5b93e9128c0feb490db27c2173493d81e9
Ep1xo <novikovworking@gmail.com>
2025-05-12 18:41:20 +0300

    header is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import './styles/header.css';
+import './styles/fonts.css';
 
 function App() {
   return (
     <>
-      
+      <header>
+        <div className="header-left-side">
+          <p className="header-left-side-logo">logo agency</p>
+
+          <div className="header-left-side-theme-switcher">
+            <div className="theme light dark"></div>
+          </div>
+        </div>
+
+        <div className="header-right-side">
+          <p className="header-right-side-menu-btn">menu</p>
+          <p className="header-right-side-say-hello-btn">say hello</p>
+        </div>
+      </header>
     </>
   );
 }
