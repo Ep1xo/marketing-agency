dd45215e9bb524ae61e021d2e5849c8849cb62e1
Ep1xo <novikovworking@gmail.com>
2025-05-12 21:21:59 +0300

    welcome block is done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './styles/header.css';
 import './styles/fonts.css';
+import './styles/welcome-block.css';
 
 function App() {
   return (
@@ -8,7 +9,6 @@ function App() {
       <header>
         <div className="header-left-side">
           <p className="header-left-side-logo">logo agency</p>
-
           <div className="header-left-side-theme-switcher">
             <div className="theme light dark"></div>
           </div>
@@ -19,6 +19,30 @@ function App() {
           <p className="header-right-side-say-hello-btn">say hello</p>
         </div>
       </header>
+
+      <div className="welcome-block">
+        <div className="welcome-block-top-part">
+          <p className="welcome-block-top-part-font-1">SPARK WAY MARKETING</p>
+          <p className="welcome-block-top-part-font-2">AGENCY</p>
+          <p className="welcome-block-top-part-font-3">Welcome to SPARK WAY, where we specialize in igniting businesses and fueling their journey to success.</p>
+        </div>
+
+        <div className="welcome-block-bottom-part">
+          <div className="welcome-block-bottom-part-left-side">
+            <p className="welcome-block-bottom-part-left-side-font-1">We are more than an agency.</p>
+            <p className="welcome-block-bottom-part-left-side-font-2">
+              <span className="welcome-block-bottom-part-left-side-font-2-2">
+                We are about: UNLOCKING POTENTIAL FUELING GROWTH SPARKING SUCCESS 
+              </span>   
+            </p>
+          </div>
+
+          <div className="welcome-block-bottom-part-right-side">
+            <p>work <br/> with <br/> us</p>
+            <span className="welcome-block-bottom-part-right-side-arrow"></span>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
