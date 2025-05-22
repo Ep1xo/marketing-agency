33ea2f3763c05654011efa362c9d5506f29d1b1d
Ep1xo <novikovworking@gmail.com>
2025-05-22 18:14:29 +0300

    removed free consultation

--- src/App.js
+++ src/App.js
@@ -32,8 +32,6 @@ function App() {
         </div>
       </header>
 
-      <FreeConsultation/>
-
       <div className="welcome-block">
         <div className="welcome-block-top-part">
           <p className="welcome-block-top-part-font-1">SPARK WAY MARKETING</p>
